c09c42e5c6d0bee19b9f0448ecc31409fa3bda33
Fahmi-Aziz <144539727+Fahmi-Aziz@users.noreply.github.com>
2024-03-06 05:00:09 +0000

    AI Open Source with LangChain

---
+++ Hf.py
@@ -0,0 +1,28 @@
+import gradio as gr
+from langchain.chains import LLMChain
+from langchain_openai import ChatOpenAI
+from langchain.prompts import PromptTemplate
+# Menggunakan Hugging FaceHub
+import os
+from langchain_community.llms import HuggingFaceEndpoint
+
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
+llm = HuggingFaceEndpoint(repo_id="google/flan-ul2")
+text = "Beritahu fakta menarik tentang kentang!"
+result = llm.invoke(text)
+print(result)
+
+def handle_complaint(komplain: str) -> str:
+    #Buat instance LLM dengan nilai temprature 0,9 (nilai lebih tinggi membuat keluaran lebih acak).
+    
+
+    # Merancang template untuk merespon komplain
+    prompt = PromptTemplate(input_variables=["komplain"], template="Saya seorang perwakilan layanan pelanggan. Saya menerima keluhan berikut: {komplain}. Respon saya adalah:")
+
+    # Membuat model bahasa berantai dengan template yang telah dirancang
+    chain = LLMChain(llm=llm, prompt=prompt)
+    return chain.run(komplain)
+
+# Membuat antarmuka Gradio untuk fungsi handle_complaint
+iface = gr.Interface(fn=handle_complaint, inputs="text", outputs="text")
+iface.launch(share=True)
---
+++ langkah_demi_langkah.py
@@ -0,0 +1,27 @@
+from langchain.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+import gradio as gr
+
+openai = ChatOpenAI(
+    model_name="gpt-3.5-turbo",
+    openai_api_key="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"
+)
+
+# Mendefinisikan PromptTemplate sebagai format prompt untuk input dari user
+prompt ="""Question: {question}
+    please provide step by step Answer based on {language} language:
+    """
+
+def generate_cover_letter(question: str, language: str) -> str:
+    formatted_prompt = prompt.format(question=question, language=language)
+    return openai.invoke(formatted_prompt).content
+
+# Define the Gradio interface inputs
+inputs = [
+    gr.Textbox(label="Question"),
+    gr.Textbox(label="Language (optional)"),
+]
+
+output = gr.Textbox(label="Output")
+
+gr.Interface(fn=generate_cover_letter, inputs=inputs, outputs=output).launch(share=True)
